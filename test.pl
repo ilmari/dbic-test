c73cb2bbeda1bfc2b72d2c313bbaa006a1782e6d
Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
2013-11-14 12:10:00 +0000

    multiple tests together

--- test.pl
+++ test.pl
@@ -5,24 +5,37 @@ use lib 'lib';
 use My::Schema;
 use Data::Printer;
 use SQL::Abstract::Tree;
+use Try::Tiny;
 use 5.18.0;
 
 # this is just for debug dump
-sub XX($) {
-    my ($rs) = @_;
+sub XX($$) {
+    my ($title,$rs) = @_;
 
-    my ($q,@bind) = @{${$rs->as_query}};
-    @bind = map { $_->[1] || '0e0' } @bind;
     state $tree = SQL::Abstract::Tree->new({
         profile=>'console',
         fill_in_placeholders => 1,
     });
+
     say '-'x30;
-    say 'Resultset:';
-    say $tree->format($q,\@bind);
+    say "$title resultset:";
+
+    try {
+        my ($q,@bind) = @{${$rs->as_query}};
+        @bind = map { $_->[1] || '0e0' } @bind;
+        say $tree->format($q,\@bind);
+    } catch {
+        say "could not build query: $_";
+    };
+    say '-'x30;
+    say 'Running it';
+    try {
+        my @ret = $rs->all;
+    } catch {
+        say "could not run it: $_";
+    };
     say '-'x30;
     say '';
-    return $rs;
 }
 
 my $schema = My::Schema->connect('dbi:SQLite:dbname=:memory:','','');
@@ -43,11 +56,7 @@ you remove the search criterion
 
 =item *
 
-you C<join> instead of C<prefetcH>
-
-=item *
-
-you C<prefetch> only C<clan>
+you C<join> instead of C<prefetch>
 
 =item *
 
@@ -61,11 +70,48 @@ you remove the C<slice>
 
 =cut
 
-my $rs = $schema->resultset('Main')->search({
-    'subs.name' => 'bar',
-},{
-    prefetch => [ 'subs' ],
-    order_by => { -asc => 'clan.name' },
-})->slice(1,5);
 
-XX $rs;
+my %rs_set = (
+    '1. no criterion' => [ {
+    },{
+        prefetch => 'subs',
+        join => 'clan',
+        order_by => { -asc => 'clan.name' },
+    } ],
+    '2. no prefetch' => [ {
+        'subs.name' => 'bar',
+    },{
+        join => ['subs','clan'],
+        order_by => { -asc => 'clan.name' },
+    } ],
+    '3. no order_by' => [ {
+        'subs.name' => 'bar',
+    },{
+        prefetch => 'subs',
+        join => 'clan',
+    } ],
+    '4. all of it' => [ {
+        'subs.name' => 'bar',
+    },{
+        prefetch => 'subs',
+        join => 'clan',
+        order_by => { -asc => 'clan.name' },
+    } ],
+    '5. all of it, different prefetch/join' => [ {
+        'subs.name' => 'bar',
+    },{
+        prefetch => 'clan',
+        join => 'subs',
+        order_by => { -asc => 'clan.name' },
+    } ],
+);
+
+for my $rs_name (sort keys %rs_set) {
+    my $rs = $schema->resultset('Main')->search(@{$rs_set{$rs_name}});
+
+    XX "$rs_name not sliced", $rs;
+
+    my $sliced_rs = $rs->slice(1,5);
+
+    XX "$rs_name sliced", $sliced_rs;
+};
